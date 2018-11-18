bbec81d6d3619cf71560cc9fc4c0bc5e8d2e8de2
Qiao-PC <qcy961011@qq.com>
2018-11-18 12:58:20 +0800

    成功配置Spring boot admin并访问页面

---
+++ admin_client/src/main/java/com/qiao/admin_client/AdminClientApplication.java
@@ -0,0 +1,14 @@
+package com.qiao.admin_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+}
